e41fc7966893e62835886aeebc1c51732a6a55c6
Dhivyadinesh <53462975+Dhivyadinesh@users.noreply.github.com>
2020-06-01 23:41:02 +0530

    Add files via upload

--- mavensel/src/main/java/SaasMainPageTesting/LoginSaasPage.java
+++ mavensel/src/main/java/SaasMainPageTesting/LoginSaasPage.java
@@ -6,18 +6,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LoginSaasPage {
+	
 	@Test
 	public void login() { 
         try {
 		System.setProperty("webdriver.gecko.driver", "/home/dinesh/Downloads/driver/geckodriver");
 		WebDriver driver = new FirefoxDriver();
-		driver.get("https://www.saasworthy.com/");
+		String url ="https://www.saasworthy.com/";
+		driver.get(url);
 		driver.manage().window().maximize();
 		driver.findElement(By.linkText("Login")).click();
-		driver.findElement(By.xpath("/html/body/div[3]/div/div/div[4]/input[1]")).sendKeys("dd@gmail.com");							
-        driver.findElement(By.xpath("/html/body/div[3]/div/div/div[4]/input[2]")).sendKeys("qazwsxedc");							
-        driver.findElement(By.xpath("/html/body/div[3]/div/div/div[4]/button")).click();					
-        System.out.println("Login Done");					
+		driver.findElement(By.xpath("/html/body/div[3]/div/div/div[4]/input[1]")).sendKeys("yashwanth@saasworthy.com");							
+        driver.findElement(By.xpath("/html/body/div[3]/div/div/div[4]/input[2]")).sendKeys("9!Mobiles");							
+        driver.findElement(By.xpath("/html/body/div[3]/div/div/div[4]/button")).click();
+        driver.findElement(By.partialLinkText("Smartsheet")).click();
+        driver.findElement(By.xpath("/html[1]/body[1]/div[11]/div[1]/div[2]/div[2]/i[1]")).click();
         }catch(Exception e) {
         	System.out.println("Unable to Login");
         }
