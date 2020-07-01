2a4ad71cc878600a0b2720374137073bd811e2b9
Dhivyadinesh <53462975+Dhivyadinesh@users.noreply.github.com>
2020-07-01 23:30:10 +0530

    Add files via upload

--- mavensel/src/main/java/mavensel/Star.java
+++ mavensel/src/main/java/mavensel/Star.java
@@ -1,21 +1,25 @@
 package mavensel;
 
+import java.util.Scanner;
+
 public class Star {
 
 	public static void main(String args[]) {
-
-		int n = 5;
+		
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n =scan.nextInt(); 
 		for (int i= 0; i<= n-1 ; i++){
 		    for (int j=0; j<=i; j++) { 
 		    	System.out.print("*"); 
 		    	} 
-		    System.out.print("\n"); 
+		    System.out.println(); 
 		}
 		for (int i=n-1; i>=0; i--) {
             for(int j=0; j <=i-1;j++){
                 System.out.print("*");
             }
-            System.out.print("\n");
+		    System.out.println(); 
 	   	}
 	}
 }
